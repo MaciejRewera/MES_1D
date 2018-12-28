d35b6d1be8abeefb013008d38781b6f365bd3336
Maciej Rewera <ReweraM@gmail.com>
2018-12-28 12:44:52 +0100

    Added structures

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+struct Node {
+    int x;
+    int BC;
+};
+
+struct Element {
+    int ID[2];
+    double S, K, L;
+    double HL[2][2];
+    double* PL;
+};
+
+struct Grid {
+    Node* nodes;
+    Element* elements;
+};
+
+struct SOE {
+    double* HG;
+    double* PG;
+    double* TG;
+};
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
